73a55626b48e54c856af990534681e90f1273a5f
Srikar Gadagoju <you@example.com>
2025-11-29 18:11:50 +0530

    skeleton route

---
+++ index.js
@@ -0,0 +1,38 @@
+const express=require('express')
+const app=express();
+app.post("/user/signup",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+app.post("/user/signin",function(req,res){
+    res.json({
+        message:"signin endpoint"
+    })
+})
+
+app.get("/user/purchases",function(req,res){
+    res.json({
+        message:"your course is purchased"
+    })
+})
+
+app.post("/course/purchase",function(req,res){
+    
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+
+app.get("/courses",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+
+
+
+app.listen(3000);
