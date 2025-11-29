3241a99643214e4448a0ca849a1189f0a05beb1a
Srikar Gadagoju <you@example.com>
2025-11-29 20:20:46 +0530

    new routers

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 const express=require('express')
 const app=express();
-app.post("/user/signup",function(req,res){
-    res.json({
-        message:"signup endpoint"
-    })
-})
+const {userRouter}=require("/routes/user")
+const {courseRouter}=require("/routes/course")
+createUserRoutes(app)
+createCourseRoutes(app)
 
-app.post("/user/signin",function(req,res){
-    res.json({
-        message:"signin endpoint"
-    })
-})
-
-app.get("/user/purchases",function(req,res){
-    res.json({
-        message:"your course is purchased"
-    })
-})
-
-app.post("/course/purchase",function(req,res){
-    
-    res.json({
-        message:"signup endpoint"
-    })
-})
+app.use("/user",userRouter);
+app.use("/course",courseRouter);
 
 
-app.get("/courses",function(req,res){
-    res.json({
-        message:"signup endpoint"
-    })
-})
 
 
 
