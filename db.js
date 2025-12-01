d456d78c8592b191d35098b3b019dc2a35ef46c7
Srikar Gadagoju <you@example.com>
2025-12-01 15:34:55 +0530

    added schema

---
+++ db.js
@@ -0,0 +1,48 @@
+const mongoose=require("mongoose");
+console.log("connected to")
+mongoose.connect("mongodb+srv://Srikar:cluster0code@cluster0.pymphhm.mongodb.net/coursera-app")
+
+const Schema=mongoose.Schema;
+const ObjectId=mongoose.Types.ObjectId;
+
+const userSchema=new Schema({
+    email:{type : String ,unique: true },
+    password:String,
+    firstname:String,
+    lastname:String,
+});
+
+const adminSchema=new Schema({
+    email:{type : String ,unique: true} ,
+    password:String,
+    firstname:String,
+    lastname:String,
+    
+});
+
+const courseschema=new Schema({
+    title:String,
+    description:String,
+    price:Number,
+    imageUrl:String,
+    creatorId:ObjectId,
+    
+});
+
+const purchaseSchema=new Schema({
+    userId:ObjectId,
+    courseId:ObjectId,
+    
+});
+
+const userModel=mongoose.model("user",userSchema);
+const adminModel=mongoose.model("admin",userSchema);
+const courseModel=mongoose.model("course",userSchema);
+const purchaseModel=mongoose.model("purchase",userSchema);
+
+module.export={
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
