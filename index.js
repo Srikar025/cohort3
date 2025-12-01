64cdb4f77614e265eaf0ce0e581f18edfd92b3a7
Srikar Gadagoju <you@example.com>
2025-12-01 15:42:17 +0530

    step7 only start if database is up

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express=require('express')
-
+const express=require('express');
+const mongoose=require("mongoose");
 const {userRouter}=require("./routes/user")
 const {courseRouter}=require("./routes/course")
-
+const {adminRouter}=require("./routes/admin")
 const app=express();
 app.use("/user",userRouter);
 app.use("/course",courseRouter);
+app.use("/admin",adminRouter);
 
 
 
+async function main() {
 
-
-
-app.listen(3000);
+    await mongoose.connect("mongodb+srv://Srikar:cluster0code@cluster0.pymphhm.mongodb.net/coursera-app")
+    app.listen(3000);
+    console.log("listening on port 3000");
+}
+main();
