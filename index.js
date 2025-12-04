9797facdd897759e8b6bd3dbc9ece5fc66a4cf84
Srikar Gadagoju <you@example.com>
2025-12-04 18:51:34 +0530

    used dotenv for environment variables

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express=require('express');
 const mongoose=require("mongoose");
 const {userRouter}=require("./routes/user")
@@ -14,7 +16,7 @@ app.use("/admin",adminRouter);
 
 async function main() {
 
-    await mongoose.connect("mongodb+srv://Srikar:cluster0code@cluster0.pymphhm.mongodb.net/coursera-app")
+    await mongoose.connect(process.env.MONGO_URL)
     app.listen(3000);
     console.log("listening on port 3000");
 }
